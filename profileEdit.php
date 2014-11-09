8ae0d6dfa9e4b1842d0aac1394d8c1e61a8b7f1e
kmdr <20.10.ak.tkb@gmail.com>
2014-11-09 15:31:00 +0900

    Profile Edit画面の編集

--- profileEdit.php
+++ profileEdit.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Template</title>
+	<title>profile Edit - cafetail</title>
 	<link rel="stylesheet" href="css/style.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <link href="css/select2.css" rel="stylesheet"/>
@@ -26,36 +26,62 @@
 		
 		<h1>Area</h1>
 
-		<p>あなたの作業エリアを指定してください。</p>
-		<select multiple name="place" id="place">
-<?php
-mb_internal_encoding("UTF-8");
-$link = mysql_connect('localhost', 'root', '');
-if (!$link) {
-    die('接続失敗です。'.mysql_error());
-}
+		<p>あなたの作業エリアを指定してください.</p>
 
-$result = mysql_query('SET NAMES UTF8;');
-$db_selected = mysql_select_db('GitNuldb', $link);
-if (!$db_selected){
-    die('データベース選択失敗です。'.mysql_error());
-}
+		<?php 
+			mb_internal_encoding("UTF-8");
+			$link = mysql_connect('localhost', 'root', '');
+			if (!$link) {
+			    die('接続失敗です。'.mysql_error());
+			}
 
-$result = mysql_query('SELECT * FROM `area`');
-if (!$result) {
-    die('クエリーが失敗しました。'.mysql_error());
-}
+			$result = mysql_query('SET NAMES UTF8;');
+			$db_selected = mysql_select_db('GitNuldb', $link);
+			if (!$db_selected){
+			    die('データベース選択失敗です。'.mysql_error());
+			}
 
-while ($row = mysql_fetch_assoc($result)) {
-    echo '<option value=\"', $row['area_name'], '\">', $row['area_name'], '</option>';
-}
+			$old_area = mysql_query("SELECT area_id from user_area where user_name = 'kmdr' group by area_id");
+			if (!$old_area) {
+			    die('クエリーが失敗しました。'.mysql_error());
+			}
 
-$close_flag = mysql_close($link);
 
-?>
-		</select><br />
-		<input id="submit_button" type="submit" name="submit" value="保存する">
+			echo "<p>すでに登録されている地域は、";
+			while($fetch = mysql_fetch_array($old_area, MYSQL_ASSOC)){
+				$old_area_name = mysql_query("SELECT area_name from area where area_id = '".$fetch['area_id']."'");
+				$old_area_name_fetch = mysql_fetch_assoc($old_area_name);
+				echo $old_area_name_fetch['area_name']. "、";
 
+			}
+
+			echo "です。</p>";
+
+			$old_area_name_fetch = mysql_fetch_assoc($old_area_name);
+			echo $old_area_name_fetch['area_name'];
+
+		?>
+
+		<form method="POST" action="profile_insert.php">
+			<select multiple name="place[]" id="place">
+			<?php
+
+
+			$result = mysql_query('SELECT * FROM `area`');
+			if (!$result) {
+			    die('クエリーが失敗しました。'.mysql_error());
+			}
+
+			while ($row = mysql_fetch_assoc($result)) {
+			    echo '<option value="', $row['area_name'], '">', $row['area_name'], '</option>';
+			}
+
+			$close_flag = mysql_close($link);
+
+			?>
+			</select><br />
+			<input id="submit_button" type="submit" name="submit" value="保存する">
+		</form>
 
 
 
---
+++ profile_insert.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Template</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="h_wrapper">
+			<div class="title">cafetail</div>
+			<div class="username"><a href="#">username</a></div>
+		</div>
+
+	</header>
+
+	<div class="wrapper">
+		
+
+		<?php 			
+			$place = $_POST['place'];
+			$result = [];
+			mb_internal_encoding("UTF-8");
+			$link = mysql_connect('localhost', 'root', '');
+			if (!$link) {
+			    die('接続失敗です。'.mysql_error());
+			}
+
+			$result = mysql_query('SET NAMES UTF8;');
+			$db_selected = mysql_select_db('GitNuldb', $link);
+			if (!$db_selected){
+			    die('データベース選択失敗です。'.mysql_error());
+			}
+			foreach ($place as $key => $value) {
+				# code...
+				echo $place[$key], "、";
+				$name = $place[$key];
+				
+				$num = mysql_query("select area_id from area where area_name = '".$place[$key]."'");
+
+				$array  = mysql_fetch_assoc($num);
+				$id = $array['area_id'];
+	
+				$sql = "INSERT INTO `GitNuldb`.`user_area` (`user_name`, `area_id`) VALUES ('kmdr', '".$id."');";
+				$result_flag = mysql_query($sql);
+
+				if (!$result_flag) {
+				    die('INSERTクエリーが失敗しました。'.mysql_error());
+				}
+
+			}
+			$close_flag = mysql_close($link);
+
+
+		?>
+		を登録しました。
+	</div>
+
+
+	<footer>
+		<div class="footer">
+			© 2014 Git NULL BOYZ
+		</div>
+	</footer>
+</body>
+</html>
